a6c48999609c6b3a4dc5fc27dbec2ba7a4e0505b
Scott Harner <scott.harner@gmail.com>
2012-07-24 22:51:18 -0400

    Split out common FlowDocument table building code.

--- code/TaskConqueror/TaskConqueror/Model/Report/ActiveTasksReport.cs
+++ code/TaskConqueror/TaskConqueror/Model/Report/ActiveTasksReport.cs
@@ -13,7 +13,7 @@ namespace TaskConqueror
     {
         #region Properties
 
-        public virtual string Title
+        public override string Title
         {
             get { return "Active Tasks"; }
         }
@@ -26,62 +26,27 @@ namespace TaskConqueror
         {
             FlowDocument flowDocument = new FlowDocument();
 
-            Table flowTable = new Table();
-            flowDocument.Blocks.Add(flowTable);
-            flowTable.CellSpacing = 10;
-            flowTable.Background = System.Windows.Media.Brushes.White;
-
-            // Create 5 columns and add them to the table's Columns collection.
-            int numberOfColumns = 5;
-            for (int x = 0; x < numberOfColumns; x++)
+            Dictionary<string, string> columnDefinitions = new Dictionary<string,string>()
             {
-                flowTable.Columns.Add(new TableColumn());
+                {"Title", "Title"},
+                {"StatusDescription", "Status"},
+                {"PriorityDescription", "Priority"},
+                {"CreatedDate", "Date Created"},
+                {"CompletedDate", "Date Completed"}
+            };
 
-                // Set alternating background colors for the middle colums.
-                if (x % 2 == 0)
-                    flowTable.Columns[x].Background = Brushes.Beige;
-                else
-                    flowTable.Columns[x].Background = Brushes.LightSteelBlue;
-            }
-
-            // Create and add an empty TableRowGroup to hold the table's Rows.
-            flowTable.RowGroups.Add(new TableRowGroup());
-
-            // Add the header row.
-            flowTable.RowGroups[0].Rows.Add(new TableRow());
-            TableRow currentRow = flowTable.RowGroups[0].Rows[0];
-
-            // Global formatting for the header row.
-            currentRow.FontSize = 18;
-            currentRow.FontWeight = FontWeights.Bold;
-
-            // Add cells with content to the second row.
-            currentRow.Cells.Add(new TableCell(new Paragraph(new Run("Title"))));
-            currentRow.Cells.Add(new TableCell(new Paragraph(new Run("Status"))));
-            currentRow.Cells.Add(new TableCell(new Paragraph(new Run("Priority"))));
-            currentRow.Cells.Add(new TableCell(new Paragraph(new Run("Date Created"))));
-            currentRow.Cells.Add(new TableCell(new Paragraph(new Run("Date Completed"))));
-
-            // set formatting for content rows.
-            currentRow.FontSize = 12;
-            currentRow.FontWeight = FontWeights.Normal;
-            int currentRowCount = 1;
-
-            // loop through content and output table rows.
             TaskData tData = new TaskData();
             List<Task> activeTasks = tData.GetActiveTasks();
-
-            foreach (Task currentTask in activeTasks)
+            List<TaskViewModel> rowData = new List<TaskViewModel>();
+            foreach (Task task in activeTasks)
             {
-                flowTable.RowGroups[0].Rows.Add(new TableRow());
-                currentRow = flowTable.RowGroups[0].Rows[currentRowCount];
-                currentRow.Cells.Add(new TableCell(new Paragraph(new Run(currentTask.Title))));
-                currentRow.Cells.Add(new TableCell(new Paragraph(new Run(currentTask.StatusId.ToString()))));
-                currentRow.Cells.Add(new TableCell(new Paragraph(new Run(currentTask.PriorityId.ToString()))));
-                currentRow.Cells.Add(new TableCell(new Paragraph(new Run(currentTask.CreatedDate.ToShortDateString()))));
-                currentRow.Cells.Add(new TableCell(new Paragraph(new Run(currentTask.CompletedDate.HasValue ? currentTask.CompletedDate.Value.ToShortDateString() : ""))));
-                currentRowCount++;
+                rowData.Add(new TaskViewModel(task, tData));
             }
+
+            flowDocument.Blocks.Add(FlowDocumentHelper.BuildTable<TaskViewModel>(columnDefinitions, rowData));
+
+            foreach (TaskViewModel taskVm in rowData)
+                taskVm.Dispose();
 
             return flowDocument;
         }
---
+++ code/TaskConqueror/TaskConqueror/Model/Report/FlowDocumentHelper.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Documents;
+using System.Windows;
+
+namespace TaskConqueror
+{
+    public class FlowDocumentHelper
+    {
+        public static Table BuildTable<T>(Dictionary<string, string> columnDefinitions, List<T> rowRecords)
+        {
+            Table flowTable = new Table();
+            flowTable.CellSpacing = 10;
+            flowTable.Background = System.Windows.Media.Brushes.White;
+
+            // Create 5 columns and add them to the table's Columns collection.
+            int numberOfColumns = columnDefinitions.Count;
+            for (int x = 0; x < numberOfColumns; x++)
+            {
+                flowTable.Columns.Add(new TableColumn());
+            }
+
+            // Create and add an empty TableRowGroup to hold the table's Rows.
+            flowTable.RowGroups.Add(new TableRowGroup());
+
+            // Add the header row.
+            flowTable.RowGroups[0].Rows.Add(new TableRow());
+            TableRow currentRow = flowTable.RowGroups[0].Rows[0];
+
+            // Global formatting for the header row.
+            currentRow.FontSize = 18;
+            currentRow.FontWeight = FontWeights.Bold;
+
+            // Add cells with content to the second row.
+            foreach (string propertyName in columnDefinitions.Keys)
+            {
+                currentRow.Cells.Add(new TableCell(new Paragraph(new Run(columnDefinitions[propertyName]))));
+            }
+
+            int currentRowCount = 1;
+
+            // loop through content and output table rows.
+            foreach (T rowRecord in rowRecords)
+            {
+                flowTable.RowGroups[0].Rows.Add(new TableRow());
+                
+                currentRow = flowTable.RowGroups[0].Rows[currentRowCount];
+                currentRow.FontSize = 12;
+                currentRow.FontWeight = FontWeights.Normal;
+
+                foreach (string propertyName in columnDefinitions.Keys)
+                {
+                    object propertyValue = rowRecord.GetType().GetProperty(propertyName).GetValue(rowRecord, null);
+                    string convertedValue = propertyValue == null ? "" : propertyValue.ToString();
+                    currentRow.Cells.Add(new TableCell(new Paragraph(new Run(convertedValue))));
+                }
+
+                currentRowCount++;
+            }
+
+            return flowTable;
+        }
+    }
+}
