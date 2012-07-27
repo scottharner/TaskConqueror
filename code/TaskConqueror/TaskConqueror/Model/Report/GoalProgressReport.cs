9a353275717edfb36e8208145f98d6429e94e1bf
Scott Harner <scott.harner@gmail.com>
2012-07-27 17:43:31 -0400

    Implemented project progress and goal progress reports.

--- code/TaskConqueror/TaskConqueror/Model/Report/GoalProgressReport.cs
+++ code/TaskConqueror/TaskConqueror/Model/Report/GoalProgressReport.cs
@@ -2,10 +2,92 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Documents;
+using System.Windows;
 
 namespace TaskConqueror
 {
     public class GoalProgressReport : ReportBase
-    {
+    {        
+        #region Fields
+
+        GoalData _gData;
+
+        #endregion
+
+        #region Constructor
+
+        public GoalProgressReport()
+        {
+            _gData = new GoalData();
+            SelectedGoal = _gData.GetGoals().FirstOrDefault();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public override string Title
+        {
+            get { return "Goal Progress"; }
+        }
+
+        public Goal SelectedGoal
+        { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public override FlowDocument Build()
+        {
+            FlowDocument flowDocument = new FlowDocument();
+
+            flowDocument.Blocks.Add(FlowDocumentHelper.BuildTitle(Title));
+
+            Paragraph criteriaPara = new Paragraph();
+            Span titleSpan = new Span();
+            titleSpan.Inlines.Add("Title: ");
+            titleSpan.FontWeight = FontWeights.Bold;
+            Span titleValueSpan = new Span();
+            titleValueSpan.Inlines.Add(SelectedGoal.Title);
+            Span dateTitleSpan = new Span();
+            dateTitleSpan.Inlines.Add("     Date Created: ");
+            dateTitleSpan.FontWeight = FontWeights.Bold;
+            Span dateValueSpan = new Span();
+            dateValueSpan.Inlines.Add(SelectedGoal.CreatedDate.ToShortDateString());
+            criteriaPara.Inlines.Add(titleSpan);
+            criteriaPara.Inlines.Add(titleValueSpan);
+            criteriaPara.Inlines.Add(dateTitleSpan);
+            criteriaPara.Inlines.Add(dateValueSpan);
+            flowDocument.Blocks.Add(criteriaPara);
+
+            Dictionary<string, string> columnDefinitions = new Dictionary<string, string>()
+            {
+                {"Title", "Project Title"},
+                {"StatusDescription", "Status"},
+                {"EstimatedCost", "Estimated Cost"},
+                {"CreatedDate", "Date Created"},
+                {"CompletedDate", "Date Completed"}
+            };
+
+            ProjectData pData = new ProjectData();
+            TaskData tData = new TaskData();
+            List<Project> childProjects = _gData.GetChildProjects(SelectedGoal.GoalId);
+            List<ProjectViewModel> rowData = new List<ProjectViewModel>();
+            foreach (Project project in childProjects)
+            {
+                rowData.Add(new ProjectViewModel(project, pData, tData));
+            }
+
+            flowDocument.Blocks.Add(FlowDocumentHelper.BuildTable<ProjectViewModel>(columnDefinitions, rowData));
+
+            foreach (ProjectViewModel projectVm in rowData)
+                projectVm.Dispose();
+
+            return flowDocument;
+        }
+
+        #endregion
     }
 }
--- code/TaskConqueror/TaskConqueror/Model/Report/ProjectProgressReport.cs
+++ code/TaskConqueror/TaskConqueror/Model/Report/ProjectProgressReport.cs
@@ -2,10 +2,91 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Documents;
+using System.Windows;
 
 namespace TaskConqueror
 {
     public class ProjectProgressReport : ReportBase
     {
+        #region Fields
+
+        ProjectData _pData;
+
+        #endregion
+
+        #region Constructor
+
+        public ProjectProgressReport()
+        {
+            _pData = new ProjectData();
+            SelectedProject = _pData.GetProjects().FirstOrDefault();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public override string Title
+        {
+            get { return "Project Progress"; }
+        }
+
+        public Project SelectedProject
+        { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public override FlowDocument Build()
+        {
+            FlowDocument flowDocument = new FlowDocument();
+
+            flowDocument.Blocks.Add(FlowDocumentHelper.BuildTitle(Title));
+
+            Paragraph criteriaPara = new Paragraph();
+            Span titleSpan = new Span();
+            titleSpan.Inlines.Add("Title: ");
+            titleSpan.FontWeight = FontWeights.Bold;
+            Span titleValueSpan = new Span();
+            titleValueSpan.Inlines.Add(SelectedProject.Title);
+            Span dateTitleSpan = new Span();
+            dateTitleSpan.Inlines.Add("     Date Created: ");
+            dateTitleSpan.FontWeight = FontWeights.Bold;
+            Span dateValueSpan = new Span();
+            dateValueSpan.Inlines.Add(SelectedProject.CreatedDate.ToShortDateString());
+            criteriaPara.Inlines.Add(titleSpan);
+            criteriaPara.Inlines.Add(titleValueSpan);
+            criteriaPara.Inlines.Add(dateTitleSpan);
+            criteriaPara.Inlines.Add(dateValueSpan);
+            flowDocument.Blocks.Add(criteriaPara);
+
+            Dictionary<string, string> columnDefinitions = new Dictionary<string, string>()
+            {
+                {"Title", "Task Title"},
+                {"StatusDescription", "Status"},
+                {"PriorityDescription", "Priority"},
+                {"CreatedDate", "Date Created"},
+                {"CompletedDate", "Date Completed"}
+            };
+
+            TaskData tData = new TaskData();
+            List<Task> childTasks = _pData.GetChildTasks(SelectedProject.ProjectId);
+            List<TaskViewModel> rowData = new List<TaskViewModel>();
+            foreach (Task task in childTasks)
+            {
+                rowData.Add(new TaskViewModel(task, tData));
+            }
+
+            flowDocument.Blocks.Add(FlowDocumentHelper.BuildTable<TaskViewModel>(columnDefinitions, rowData));
+
+            foreach (TaskViewModel taskVm in rowData)
+                taskVm.Dispose();
+
+            return flowDocument;
+        }
+
+        #endregion
     }
 }
