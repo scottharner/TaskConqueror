25a1fedc50842fdd589a549ea3e25c0012a958ea
Scott Harner <scott.harner@gmail.com>
2012-08-01 22:49:23 -0400

    Began setting up image buttons.

---
+++ code/TaskConqueror/TaskConqueror/View/ImageButton.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskConqueror
+{
+    /// <summary>
+    /// Interaction logic for ImageButton.xaml
+    /// </summary>
+    public partial class ImageButton : UserControl
+    {
+        public ImageButton()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(ImageButton), new PropertyMetadata((string)""));
+        public string Text
+        {
+            get { return (string)this.GetValue(TextProperty); }
+            set { this.SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register("Image", typeof(ImageSource), typeof(ImageButton), new PropertyMetadata());
+        public ImageSource Image
+        {
+            get { return (ImageSource)this.GetValue(ImageProperty); }
+            set { this.SetValue(ImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register("Command", typeof(ICommand), typeof(ImageButton), new PropertyMetadata());
+        public ICommand Command
+        {
+            get { return (ICommand)this.GetValue(CommandProperty); }
+            set { this.SetValue(CommandProperty, value); }
+        }
+
+    }
+}
